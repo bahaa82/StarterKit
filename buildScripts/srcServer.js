afd5335243f4caa62fb436ae2886d6f59bc7b0b7
bahaa82 <bahaa1982@gmail.com>
2018-09-17 17:09:55 +0300

    webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import path from'path';
 import open from'open';
-
+import webpack from'webpack';
+import config from '../webpack.config.dev';
 const port = 8000;
 
 const app = express();
